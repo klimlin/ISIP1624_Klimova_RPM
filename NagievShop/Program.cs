94d969b608b270c18e793631b40c9a6fbe089201
klimlin <linalevel5@gmail.com>
2026-01-28 12:19:09 +0300

    Подключила БД, вывела список товаров из БД в консоль

---
+++ NagievShop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NagievShop
+{
+    internal class Program
+    {
+        static List<Item> items = Core.Context.Item.ToList();
+
+
+
+        static void Main(string[] args)
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine($"ID: {item.ID} Название товара: {item.Name} Цена: {item.Price:F2} руб.");
+            }
+        }
+    }
+}
